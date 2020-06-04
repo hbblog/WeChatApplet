b1b649f132e6603a146095e85f004cc8fe71734d
hbblog <854898783@qq.com>
2020-06-04 22:59:31 +0800

    11

--- pages/newlist/newlist.js
+++ pages/newlist/newlist.js
@@ -18,54 +18,10 @@ Page({
       {
         id: 3,
         name: "视频"
-      },
-      // {
-      //   id: 4,
-      //   name: "直播"
-      // },
-      // {
-      //   id: 5,
-      //   name: "影视"
-      // },
-      // {
-      //   id: 6,
-      //   name: "游戏"
-      // },
-      // {
-      //   id: 7,
-      //   name: "新闻"
-      // }
+      }
     ],
     selectType: 1,
-    newsList: [
-      //  {
-      //   id: 1,
-      //   userName: "HB",
-      //   headUrl: "/pages/resource/头像.jpg",
-      //   isFocus: true,
-      //   newsTitle: "只争朝夕，不负韶华",
-      //   isVideo: true,
-      //   videoUrl: "/pages/resource/test.mp4",
-      //   newsText: null,
-      //   newAbstrack: null,
-      //   imageUrl: null,
-      //   isOriginal: true,
-      //   createTime: "2020-04-29T09:45:15",
-      // }, {
-      //   id: 2,
-      //   userName: "HB",
-      //   headUrl: "/pages/resource/title1.jpg",
-      //   isFocus: false,
-      //   newsTitle: "",
-      //   isVideo: false,
-      //   videoUrl: null,
-      //   newsText: "尼日尔东下回合，欧拉函数那地方的省份许我天若有情拉看法是在考虑现代化反好",
-      //   newAbstrack: "简介",
-      //   imageUrl: "/pages/resource/title1.jpg",
-      //   isOriginal: false,
-      //   createTime: "2020-04-29T10:45:15",
-      // }
-    ],
+    newsList: [],
   },
 
   selectBar(e) {
